6c0c91251db9907a88c9e50aad1626da9cb87a27
Joh Yoshida <johyoshida@gmail.com>
2022-05-20 17:14:08 -0700

    Super rudimentary interest list

--- screens/InterestsScreen.tsx
+++ screens/InterestsScreen.tsx
@@ -4,12 +4,47 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
+const SportsList = [
+  "Soccer",
+  "Basketball",
+  "Tennis",
+  "Baseball",
+  "Golf",
+  "Running",
+  "Volleyball",
+  "Swimming",
+  "Boxing",
+  "Skiing"
+];
+
+const SportsBool = [
+  0, 1, 0, 0, 1, 0, 1, 0, 0, 1
+];
+
+const InterestsList = [
+  SportsList
+];
 
 export default function InterestsScreen({ navigation }: RootTabScreenProps<'Interests'>) {
+  let Interests = [];
+  console.log(InterestsList);
+  SportsList.forEach((element, index) => {
+    Interests.push(
+      <Button
+        title={element}
+        color={SportsBool[index] == 0 ? "#d91212" : "#15e43c"}
+        onPress={() => SportsBool[index] == 0 ? SportsBool[index] = 1 : SportsBool[index] = 0}
+      />
+    );
+  });
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Interests</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <ScrollView>
+        {Interests}
+      </ScrollView>
     </View>
   );
 }
